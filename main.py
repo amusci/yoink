c912daf42ec60a3223dcdc392c65202e5d105ce8
a <antoniomusciano@gmail.com>
2024-10-08 20:52:26 -0400

    newgrounds functionality

--- main.py
+++ main.py
@@ -45,6 +45,28 @@ def startXDownload():
     except Exception as e:
         print(f"Error: {e}")
 
+def startNGDownload():
+
+    # Function to download NG videos
+
+    ngLink = ng_link.get()
+    download_path = "C:/coding/mainprojects/yoink/DownloadedItems/NG"
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    try:
+        
+        download_options = {
+            'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s')  
+        }
+
+        
+        with yt_dlp.YoutubeDL(download_options) as ydl:
+            ydl.download([ngLink])
+            print(f"Download Complete! Saved to: {download_path}")
+    except Exception as e:
+        print(f"Error: {e}")
+
 # System Settings
 
 ctk.set_appearance_mode("System")
@@ -53,13 +75,13 @@ ctk.set_default_color_theme("blue")
 # App Frame
 
 app = ctk.CTk()
-app.geometry("1080x600")
+app.geometry("1080x900")
 app.title("yoink")
 
 # YT UI 
 
 yt_title = ctk.CTkLabel(app, text="Insert A Youtube Video/Shorts Link")
-yt_title.pack(padx=10, pady=10)
+yt_title.pack(padx=10, pady=50)
 
 # YT Link
 
@@ -74,7 +96,7 @@ download_yt.pack(padx=10, pady=10)
 # X UI 
 
 x_title = ctk.CTkLabel(app, text="Insert A X Video Link")
-x_title.pack(padx=10, pady=10)
+x_title.pack(padx=10, pady=50)
 
 # X Link
 
@@ -85,6 +107,21 @@ x_link.pack(padx=10, pady=10)
 # Download X Button
 download_x = ctk.CTkButton(app, text="Download X", command=startXDownload)
 download_x.pack(padx=10, pady=10)
+
+# NG UI 
+
+ng_title = ctk.CTkLabel(app, text="Insert A Newgrounds Link")
+ng_title.pack(padx=10, pady=50)
+
+# NG Link
+
+ng_url = tk.StringVar()
+ng_link = ctk.CTkEntry(app, width=350, height=40)
+ng_link.pack(padx=10, pady=10)
+
+# Download NG Button
+download_ng = ctk.CTkButton(app, text="Download NG", command=startNGDownload)
+download_ng.pack(padx=10, pady=10)
 
 # Main Loop
 
